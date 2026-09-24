806289ef43c3f9174996d07a4f38011e4c8ef615
SCMNomad <44250630+SCMNomad@users.noreply.github.com>
2026-09-24 13:16:51 -0500

    Register creature type diagnostics menu

--- scripts/settings.js
+++ scripts/settings.js
@@ -1,9 +1,18 @@
 import * as MODULE_CONFIG from "./config.js";
 import { Utils } from "./utils.js";
 import { registerCreatureTypeSetting } from "./creature-type.js";
+import { CreatureTypeDebugMenu } from "./creature-type-debug-menu.js";
 
 export function registerSettings() {
     registerCreatureTypeSetting();
+    Utils.registerMenu("creatureTypeDiagnostics", {
+        name: "HNN.Settings.CreatureTypeDebugN",
+        label: "HNN.Settings.CreatureTypeDebugLabel",
+        hint: "HNN.Settings.CreatureTypeDebugH",
+        icon: "fas fa-bug",
+        type: CreatureTypeDebugMenu,
+        restricted: true
+    });
 
     Utils.registerSetting(MODULE_CONFIG.SETTING_KEYS.hideHostile, {
         name: "HNN.Settings.HideHostileN",
